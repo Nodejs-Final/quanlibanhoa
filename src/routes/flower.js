11ce5d4af8e18c8b3290b702b739a5055b4ca630
SaDo21042000 <dovansa21042000@gmail.com>
2021-05-29 09:29:29 +0700

    dff

--- src/routes/flower.js
+++ src/routes/flower.js
@@ -4,15 +4,10 @@ const router = express.Router()
 const flowerController = require('../controllers/flowerController')
 
 router.get('/', flowerController.home)
-<<<<<<< HEAD
-router.post('/create/flower', flowerController.createFlower)
-router.get('/create', flowerController.create)
-=======
 router.get('/page-detail', flowerController.pageDetail)
 router.get('/register', flowerController.register)
 router.get('/search', flowerController.search)
 
 
->>>>>>> e3032fcfe4e775a55a4fb318eed794176fa038ce
 
 module.exports = router 
