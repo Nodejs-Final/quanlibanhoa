c64ff236541f5699b9ac8df77876344acccdbf14
SaDo21042000 <dovansa21042000@gmail.com>
2021-05-29 08:24:01 +0700

    render page

--- src/controllers/flowerController.js
+++ src/controllers/flowerController.js
@@ -1,7 +1,19 @@
 class flowerController{
     home(req, res, next){
-        res.render('user/index')
+        res.render('trang_1')
     }
-}
+
+    pageDetail(req, res, next) {
+        res.render('trang_chi_tiet_hoa')
+    }
+
+    register(req, res, next) {
+        res.render('trang_dang_ky')
+    }
+
+    search(req, res, next) {
+        res.render('trang_tim_kiem')
+    }   
+ }
 
 module.exports = new flowerController
--- src/routes/flower.js
+++ src/routes/flower.js
@@ -4,5 +4,10 @@ const router = express.Router()
 const flowerController = require('../controllers/flowerController')
 
 router.get('/', flowerController.home)
+router.get('/page-detail', flowerController.pageDetail)
+router.get('/register', flowerController.register)
+router.get('/search', flowerController.search)
+
+
 
 module.exports = router 
